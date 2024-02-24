7529d6828a14a254c24edb75cd2af85bbc8018d1
jlib245 <85942624+jlib245@users.noreply.github.com>
2024-02-24 13:20:26 +0900

    상점화면 작업중...

--- Assets/Shop Scene UI Script/Buy Button Script.cs
+++ Assets/Shop Scene UI Script/Buy Button Script.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BuyButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
