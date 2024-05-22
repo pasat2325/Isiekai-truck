913e28d988c543f54b66e9687bb965182fe8d925
Vomeee <ye43019@gmail.com>
2024-05-22 18:08:23 +0900

    5/22 yuusha algo

--- Assets/Yuusha_Prefab/Yuusha_Movement.cs
+++ Assets/Yuusha_Prefab/Yuusha_Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Yuusha_Movement : MonoBehaviour
 {
     public SphereCollider collider;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,6 @@ public class Yuusha_Movement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        rb.velocity = new Vector3(0, 0, 0);
     }
 }
